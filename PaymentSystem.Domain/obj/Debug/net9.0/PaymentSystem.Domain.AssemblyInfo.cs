865bae6fa908bc0d6ff62b7ca43e6821305b35c5
Eduardo <dudue0578@gmail.com>
2025-08-04 23:38:08 -0300

    remoção classe inútil

--- PaymentSystem.Domain/obj/Debug/net9.0/PaymentSystem.Domain.AssemblyInfo.cs
+++ PaymentSystem.Domain/obj/Debug/net9.0/PaymentSystem.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentSystem.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+022878d3abf7bc1c8d22b177be08e766b1488e80")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentSystem.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentSystem.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
